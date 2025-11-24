40f4a5d672eb38188ad662c143cdd4572d7fb2c4
GOTICOMASTER <di.montecinos@duocuc.cl>
2025-11-24 17:53:46 -0300

    sexto commit: subir a servidor

--- helpdesk_chatbot/chatbot/admin.py
+++ helpdesk_chatbot/chatbot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ticket
 
-# Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre_usuario", "correo", "asunto", "estado", "fecha_creacion")
+    list_filter = ("estado", "fecha_creacion")
+    search_fields = ("nombre_usuario", "correo", "asunto", "descripcion")
+    ordering = ("-fecha_creacion",)
